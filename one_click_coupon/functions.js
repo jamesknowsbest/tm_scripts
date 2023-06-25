b861a317557238e0617a96473039bdafe1e7d0a2
James Palmisano <jamesmpalmisano@gmail.com>
2023-06-25 10:04:52 -0400

    added wait logic, scrolling logic, and updated btn txt

--- one_click_coupon/functions.js
+++ one_click_coupon/functions.js
@@ -12,7 +12,8 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
  */
 const findAllCoupons = function () {
     // scroll to bottom of the page to load all coupons
-
+    let root = document.getElementById("root");
+    window.scrollTo(0, root.scrollHeight);
     // query for all coupons on the page
     let coupons = document.querySelectorAll("#content > section > div > section.relative.pt-48 > section > section > div > div.CouponsCatalogue-bottomContent.flex.items-start > div.CouponsCatalogue-coupons.flex-1.flex.flex-col.overflow-hidden > div > div > div > ul > li > div > div > div > div > div.CouponCard-ButtonSection > button:nth-child(2)");
     // return all coupon buttons as an array of button elements
@@ -35,6 +36,7 @@ const userIsLoggedIn = function () {
 const clipAllCoupons = async function () {
     //check if user is logged in first and if not return false and display prompt
     if (!userIsLoggedIn) {
+        console.log("user is not logged in");
         GM_notification({
             text: "Hi, you are not logged in, please log in to clip coupons",
             title: "Please log in",
@@ -45,15 +47,19 @@ const clipAllCoupons = async function () {
     }
     let coupons = findAllCoupons();
     coupons = Array.prototype.slice.call(coupons);
+    let totalNumOfCoupons = coupons.length;
+    let clipAllBtnInnerTxt = document.getElementById("clipAllBtnInnerTxt");
     try {
+        clipAllBtnInnerTxt.innerText = `clipping... 0% clipped`;
         for (const coupon of coupons) {
             // for every 5th coupon wait 5 seconds to help avoid throttles
             if (coupons.indexOf(coupon) % 5 === 0) {
                 await delay(5000);
-                // TODO update button text to show progress 
+                clipAllBtnInnerTxt.innerText = `clipping... ${coupons.indexOf(coupon)/totalNumOfCoupons}% clipped`;
             }
             coupon.click();
         }
+        clipAllBtnInnerTxt.innerText = `Finished clipping, happy shopping!`;
     } catch (error) {
         console.error(error);
         return false;
@@ -62,17 +68,40 @@ const clipAllCoupons = async function () {
 };
 
 /**
+ * waits for element to exist on the page
+ * source https://stackoverflow.com/a/61511955
+ */
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+};
+
+/**
  * create button on the page with styles
  */
 const createClipAllBtn = async function () {
-    await delay(5000);
-    let btnContainer = document.querySelector("#content > section > div > section > section > div");
+    let btnContainer = await waitForElm("#content > section > div > section > section > div");
     let clipAllBtn = GM_addElement(btnContainer, 'button', {
         id : "clipAllBtn",
         innerText : "Clip all coupons displayed",
         class : "kds-Tabs-tab interactive kind-dominant variant-fill palette-accent"
     });
     clipAllBtn.addEventListener('click', clipAllCoupons, false);
-    clipAllBtn.innerHTML = "<span class=\"kds-Text--l tab-text\">Clip All Coupons</span>";
+    clipAllBtn.innerHTML = "<span id=\"clipAllBtnInnerTxt\" class=\"kds-Text--l tab-text\">Clip All Coupons</span>";
     return clipAllBtn;
 };
